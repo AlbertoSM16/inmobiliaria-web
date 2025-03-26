6beef5add1eb3709816c947d554f0788f91d2c1e
Alberto Siles <albertosiles2001@gmail.com>
2025-03-26 19:08:09 +0100

    searcher and interface done, aos install

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; 
 @Component({
   selector: 'buscador',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './buscador.component.html',
   styleUrl: './buscador.component.css'
 })
 export class BuscadorComponent {
-
+  tipoContrato: string = "";
+  tipoInmueble: string = "";
 }
--- src/app/pages/houses-list/houses-list.component.ts
+++ src/app/pages/houses-list/houses-list.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { BuscadorComponent } from '../../components/buscador/buscador.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-houses-list',
-  imports: [BuscadorComponent],
+  imports: [BuscadorComponent,HeaderComponent],
   templateUrl: './houses-list.component.html',
   styleUrl: './houses-list.component.css'
 })
 export class HousesListComponent {
-
+  
 }
